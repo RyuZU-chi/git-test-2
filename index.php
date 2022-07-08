89561d5b9b8c8c78ce8e9e26873c1ca8b7da9135
RyuZU-chi <kuzukenji12@gmail.com>
2022-07-08 11:02:15 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
 	<h1>Hello World</h1>
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, sint quia optio iusto perspiciatis totam. Quod, maxime exercitationem? Praesentium reiciendis vel nostrum quia magni a amet nihil. Quidem voluptate quos praesentium blanditiis, perferendis ratione ipsum rerum fugiat officia dicta doloribus ex sapiente, dolorum ea accusantium dolores soluta sint. Temporibus rerum, ratione placeat sapiente, facere impedit omnis praesentium nulla atque a sint obcaecati voluptas quidem excepturi enim nemo perspiciatis possimus voluptates vero veniam consequuntur et aperiam. Est sed omnis perferendis ducimus eligendi, cumque dolore? Sequi, beatae porro, ab itaque voluptates officia excepturi voluptatem sunt, quisquam quo doloribus? Similique officiis animi itaque?</p>
 </body>
 </html>
